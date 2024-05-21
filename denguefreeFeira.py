b1c87d2075701419664a490e317e60546bd624bc
leonardogamateixeira <leoteixeirastack@outlook.com>
2024-05-21 16:43:20 -0300

    adciona declaração de não plagio

--- denguefreeFeira.py
+++ denguefreeFeira.py
@@ -1,3 +1,12 @@
+# Autor: Leonardo Gama Teixeira
+# Componente Curricular: 2024.1 EXA854 - MI - ALGORITMOS (TP03) 
+# Concluido em: 15/04/2024
+# Declaro que este código foi elaborado por mim de forma individual e não contém nenhum
+# trecho de código de outro colega ou de outro autor, tais como provindos de livros e
+# apostilas, e páginas ou documentos eletrônicos da Internet. Qualquer trecho de código
+# de outra autoria que não a minha está destacado com uma citação para o autor e a fonte
+# do código, e estou ciente que estes trechos não serão considerados para fins de avaliação.
+
 import csv
 
 InvalidOp = True
@@ -18,10 +27,7 @@ Selecione uma opção: ''')
                 for row in reader:
                     print(' '.join(row))
         case "3":
-            escrever = input("Escreva: ")
-            with open('some.csv', 'w', newline='') as csvfile:
-                writer = csv.writer(csvfile)
-                writer.writerows(escrever)
+            print("Escreva")
         case "4":
             print("\nFinalizando programa")
             exit()

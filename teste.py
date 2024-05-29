3b401fdd046db6dfe9c90644d0dd846a377ef95e
leonardogamateixeira <leoteixeirastack@outlook.com>
2024-05-29 00:08:00 -0300

    tirando inutilidades

--- teste.py
+++
@@ -1,4 +0,0 @@
-confSoma1 = sum([int(row[5]) for row in Lista if row[rowFilt] == filtro])
-confSoma2 = sum([int(row[5]) for row in Lista if row[rowFilt] == filtro2])
-negSoma1 = sum([int(row[4]) for row in Lista if row[rowFilt] == filtro])
-negSoma2 = sum([int(row[4]) for row in Lista if row[rowFilt] == filtro2])
